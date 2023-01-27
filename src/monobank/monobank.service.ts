ae97b4e20a74ce8a82b94cc17e4ced46a06dc7c4
YuriiKovalov <kovalovy76@gmail.com>
2023-01-27 11:15:29 +0200

    update

--- src/monobank/monobank.service.ts
+++ src/monobank/monobank.service.ts
@@ -9,10 +9,31 @@ export class MonobankService {
     this.store = firebaseApp.firestore();
   }
   async create(createMonobankDto: any) {
+    // {
+    //   type: 'StatementItem',
+    //     data: {
+    //   account: 'uIMmOh8y9X8j26TTDeMBbQ',
+    //     statementItem: {
+    //     id: 'bwHBTrvHBR01n8P_',
+    //       time: 1674810855,
+    //       description: 'Юра Ковальов',
+    //       mcc: 4829,
+    //       originalMcc: 4829,
+    //       amount: -100,
+    //       operationAmount: -100,
+    //       currencyCode: 980,
+    //       commissionRate: 0,
+    //       cashbackAmount: 0,
+    //       balance: 2382425,
+    //       hold: true,
+    //       receiptId: 'K442-1M9M-C019-2BHE'
+    //   }
+    // }
+    // }
     console.log(createMonobankDto);
     return await this.store
       .collection('bufferMonobank')
-      .doc(createMonobankDto.data.statementItem)
+      .doc(createMonobankDto.data.statementItem.id)
       .set(createMonobankDto);
   }
 }
