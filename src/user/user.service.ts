77e961496f60187b2fad99d4e2ffe49326e41a93
YuriiKovalov <kovalovy76@gmail.com>
2023-03-20 16:50:48 +0200

    upd

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Guid } from 'typescript-guid';
 
 import { User, UserDocument } from './schema/user.schema';
 import { CategoryDto, UserDto } from './dto/user.dto';
 import { Spending, SpendingDocument } from '../spending/schema/spending.schema';
-import { Guid } from 'typescript-guid';
 
 @Injectable()
 export class UserService {
