bd2b662683f5adc9f1970d7cd9af3beaf7133bfc
yuriikovalov1993 <79081359+YuriiKovalov@users.noreply.github.com>
2023-04-03 09:41:03 +0300

    multi-update (#10)

--- src/spending/spending.service.ts
+++ src/spending/spending.service.ts
@@ -12,13 +12,15 @@ import { SpendingStatusEnum } from '../shared/enums/enums';
 
 @Injectable()
 export class SpendingService {
-
   constructor(
     @InjectModel(Spending.name) private spendingModel: Model<SpendingDocument>,
-    @InjectModel(User.name) private userModel: Model<UserDocument>
-  ) { }
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
 
-  async create(createSpendingDto: SpendingDto, userId: string): Promise<Spending> {
+  async create(
+    createSpendingDto: SpendingDto,
+    userId: string,
+  ): Promise<Spending> {
     const time = Math.floor(new Date().getTime() / 1000);
     const spending = {
       ...createSpendingDto,
@@ -31,36 +33,68 @@ export class SpendingService {
     return await new this.spendingModel(spending).save();
   }
 
-  async findAll(userId: string, params?: GetSpendingQueryDto): Promise<Spending[]> {
+  async findAll(
+    userId: string,
+    params?: GetSpendingQueryDto,
+  ): Promise<Spending[]> {
     const query = { userId, status: SpendingStatusEnum.Accepted };
-    if (params) Object.assign(query, { time: { $gte: +params.startDate, $lte: +params.endDate } });
+    if (params)
+      Object.assign(query, {
+        time: { $gte: +params.startDate, $lte: +params.endDate },
+      });
     return this.spendingModel.find(query).exec();
   }
 
   async findAllDeleted(userId: string): Promise<Spending[]> {
-    return this.spendingModel.find({ userId, status: SpendingStatusEnum.Rejected }).exec();
+    return this.spendingModel
+      .find({ userId, status: SpendingStatusEnum.Rejected })
+      .exec();
   }
 
   async findAllPending(userId: string): Promise<Spending[]> {
     const user = await this.userModel.findOne({ id: userId }).exec();
-    const accounts = user?.monoBankAccounts.map(account => account.id);
+    const accounts = user?.monoBankAccounts.map((account) => account.id);
     if (!accounts?.length) return [];
-    const data = (await this.spendingModel.find({ userId, status: SpendingStatusEnum.Pending })
-      .exec())?.filter(transaction => accounts?.includes(transaction.accountId));
+    const data = (
+      await this.spendingModel
+        .find({ userId, status: SpendingStatusEnum.Pending })
+        .exec()
+    )?.filter((transaction) => accounts?.includes(transaction.accountId));
     return data.length ? data : [];
   }
 
-  async update(id: string, updateSpendingDto: SpendingDto, userId: string): Promise<Spending> {
+  async update(
+    id: string,
+    updateSpendingDto: SpendingDto,
+    userId: string,
+  ): Promise<Spending> {
     return await this.spendingModel
       .findOneAndUpdate({ id, userId }, updateSpendingDto)
       .exec();
   }
 
   async delete(id: string, userId: string): Promise<void> {
-    await this.spendingModel.findOneAndUpdate({ id, userId }, {
-      status: SpendingStatusEnum.Rejected,
-      removalTime: Math.floor(new Date().getTime() / 1000)
-    }).exec();
+    await this.spendingModel
+      .findOneAndUpdate(
+        { id, userId },
+        {
+          status: SpendingStatusEnum.Rejected,
+          removalTime: Math.floor(new Date().getTime() / 1000),
+        },
+      )
+      .exec();
+  }
+
+  async deleteByIds(ids: string[], userId: string): Promise<void> {
+    await this.spendingModel
+      .updateMany(
+        { userId, id: { $in: ids } },
+        {
+          status: SpendingStatusEnum.Rejected,
+          removalTime: Math.floor(new Date().getTime() / 1000),
+        },
+      )
+      .exec();
   }
 
   async hardDelete(id: string, userId: string): Promise<void> {
@@ -68,12 +102,17 @@ export class SpendingService {
   }
 
   async hardDeleteAllRejected(userId: string): Promise<void> {
-    await this.spendingModel.remove({ userId, status: SpendingStatusEnum.Rejected }).exec();
+    await this.spendingModel
+      .remove({ userId, status: SpendingStatusEnum.Rejected })
+      .exec();
   }
 
   @Cron(CronExpression.EVERY_DAY_AT_3AM)
   async removeMonthAndOlderSpending(): Promise<void> {
     const dateBreaker = Math.floor((new Date().getTime() - 2592000000) / 1000);
-    await this.spendingModel.remove({ status: SpendingStatusEnum.Rejected, removalTime: { $lte: +dateBreaker }});
+    await this.spendingModel.remove({
+      status: SpendingStatusEnum.Rejected,
+      removalTime: { $lte: +dateBreaker },
+    });
   }
 }
