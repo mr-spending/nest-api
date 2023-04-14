c45a7335123a4079be35feb7a2bebe7c9d64bb21
YuriiKovalov <kovalovy76@gmail.com>
2023-04-14 16:12:04 +0300

    multi-update

--- src/monobank/monobank.controller.ts
+++ src/monobank/monobank.controller.ts
@@ -6,23 +6,31 @@ import { CreateMonobankDto } from './dto/create-monobank.dto';
 import { UserTokenData } from '../shared/interfaces/user';
 import { Spending } from '../spending/schema/spending.schema';
 import { SpendingDto } from '../spending/dto/spending.dto';
+import { Server } from "socket.io";
+import { AppGateway } from "../websocket-gateway/websocket-gateway.gateway";
 
 @ApiTags('Monobank')
 @ApiBearerAuth()
 @Controller('monobank')
 export class MonobankController {
 
-  constructor(private readonly monobankService: MonobankService) { }
+  constructor(
+    private readonly monobankService: MonobankService,
 
-  @Get()
+  ) { }
+
+  @Post('/test')
   @HttpCode(200)
   @ApiResponse({ status: 200 })
-  async monoTest() { }
+  async test(@Body() body: { name: string }) {
+    this.monobankService.test(body.name);
+  }
 
   @Post()
   @HttpCode(200)
   @ApiResponse({ status: 200 })
   async create(@Body() createMonobankDto: CreateMonobankDto): Promise<any> {
+    this.monobankService.test('updated');
     return await this.monobankService.create(createMonobankDto);
   }
 
