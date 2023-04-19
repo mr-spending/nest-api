c50662f6d0ca0bdb3ed63380e597936371db3f9e
YuriiKovalov <kovalovy76@gmail.com>
2023-04-19 12:54:40 +0300

    main settings

--- src/main.ts
+++ src/main.ts
@@ -3,32 +3,26 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
-import { iconSwagger } from '../settings/swagger.settings';
+import {
+  listenPort,
+  swaggerCustomOptions,
+  swaggerDocumentConfig,
+  swaggerPath,
+} from '../settings/main.settings';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle('Spending app')
-    .setDescription('The spending API description')
-    .setVersion('1.0dev')
+    .setTitle(swaggerDocumentConfig.title)
+    .setDescription(swaggerDocumentConfig.description)
+    .setVersion(swaggerDocumentConfig.version)
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document
-    , {
-    customfavIcon: iconSwagger,
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.0/swagger-ui-bundle.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.0/swagger-ui-standalone-preset.min.js',
-    ],
-    customCssUrl: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.0/swagger-ui.css',
-    ],
-  }
-  );
-  
-  await app.listen(3500);
+  SwaggerModule.setup(swaggerPath, app, document, swaggerCustomOptions);
+
+  await app.listen(listenPort);
 }
 bootstrap();
